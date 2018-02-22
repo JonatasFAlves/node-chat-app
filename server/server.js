5127a7a26662272b51675daa676f925a54cec84f
JonatasFAlves <alves.jnts@gmail.com>
2018-02-22 00:37:22 -0300

    Added basic connection using sockets

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app)
+const io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 
+app.use(express.static(publicPath));
+
+io.on('connection', () => console.log('Client connected'));
+
+server.listen(3000, () => console.log('Server started'));
