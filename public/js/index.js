03f56f696f25fc5cc479e8d198b99f546593e91b
JonatasFAlves <alves.jnts@gmail.com>
2018-02-23 22:11:13 -0300

    Sending and listening to events in client and server

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
 var socket = io();
 
+socket.on('newMessage', function(message) {
 
+});
+
+socket.on('connect', function() {
+    socket.emit('createMessage', {
+        from: 'me, the client',
+        text: 'Hey server... Shut up..'
+    });
+});
 
--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-io.on('connection', () => console.log('Client connected'));
+io.on('connection', (socket) => {
+    socket.emit('newMessage', {
+        from: 'J-Server',
+        text: 'Hey man, this is your server speaking.',
+        createdAt: new Date()
+    });
+
+    socket.on('createMessage', (message) => console.log(message));
+});
 
 server.listen(3000, () => console.log('Server started'));
